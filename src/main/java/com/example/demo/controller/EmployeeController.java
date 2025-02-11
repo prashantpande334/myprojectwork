19182ee4eed18979306cf41e6b00512317f7dd68
prashantpande334 <prashantpande334@gmail.com>
2025-02-11 19:18:06 +0530

    my second commit in my frsit project

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class EmployeeController {
 	
-	
+	@GetMapping("/welcome")
 	public String mymethod() {
 		
 		return "my project ";

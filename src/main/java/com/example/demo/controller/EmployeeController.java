40954c6de3cec9f8076ccdc189e66fcfe6305fb8
prashantpande334 <prashantpande334@gmail.com>
2025-02-11 18:56:56 +0530

    my frist project and my frist commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class EmployeeController {
+	
+	
+	public String mymethod() {
+		
+		return "my project ";
+	}
+
+}
